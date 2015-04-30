ec08f6948cb17193713b447a20518e740c0d8b49
Zach Moore <zcmoore@asu.edu>
2015-04-29 21:41:08 -0700

    Data Model Placeholders

---
+++ src/edu/asu/ser322/data/User.java
@@ -0,0 +1,56 @@
+package edu.asu.ser322.data;
+
+/**
+ * Placeholder.
+ * 
+ * Model of a single user, identified by their username. 
+ * <p>
+ * NOTE: the inclusion of
+ * {@link #password} is a potential security issue, and will likely be removed,
+ * deprecated, or repurposed in a future version. It is currently included for
+ * completeness.
+ * 
+ * @author Moore, Zachary
+ *
+ */
+public class User
+{
+	public static final User NULL_USER = new User();
+	
+	private String username;
+	private String password;
+	private Character waifu;
+	
+	// TODO: Populate
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	
+	public Character getWaifu()
+	{
+		return waifu;
+	}
+	
+	public void setWaifu(Character waifu)
+	{
+		this.waifu = waifu;
+	}
+	
+}
