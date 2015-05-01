0236a1b043a8fb20e6741e8d6db7902b691578d5
COsorio11 <cosorio@asu.edu>
2015-05-01 13:19:54 -0700

    very small addition to main menu

--- src/edu/asu/ser322/MainMenuGUI.java
+++ src/edu/asu/ser322/MainMenuGUI.java
@@ -8,6 +8,18 @@ import javax.swing.JPanel;
 public class MainMenuGUI extends JPanel
 {
 	Client client;
+
+    private JLabel TitleLabel;
+    private JLabel AccounInfoLabel;
+
+    private JComboBox ListOfTables;
+    private ArrayList<String> LOT = ArrayList<String>();
+
+    private JTextField searchTextBar;
+
+    private JButton Settings;
+    private JButton SearchButton;
+
     public MainMenuGUI(Client client)
     {
     	this.client = client;
