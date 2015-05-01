b77370e390fb0305e6325e6c35b0e65e6ee46da7
Zach Moore <zcmoore@asu.edu>
2015-04-30 19:03:09 -0700

    Added Character Data Model

--- src/edu/asu/ser322/data/Character.java
+++ src/edu/asu/ser322/data/Character.java
@@ -1,5 +1,7 @@
 package edu.asu.ser322.data;
 
+import java.util.Date;
+
 /**
  * Placeholder.
  * 
@@ -11,10 +13,81 @@ package edu.asu.ser322.data;
 public class Character
 {
 	private int id;
+	private Gender gender;
+	private String archetype;
+	private String name;
+	private String hairColour;
+	private Date birthDate;
 	
 	public int getID()
 	{
 		return id;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public Gender getGender()
+	{
+		return gender;
+	}
+	
+	public void setGender(Gender gender)
+	{
+		this.gender = gender;
+	}
+	
+	public String getArchetype()
+	{
+		return archetype;
+	}
+	
+	public void setArchetype(String archetype)
+	{
+		this.archetype = archetype;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getHairColour()
+	{
+		return hairColour;
+	}
+	
+	public void setHairColour(String hairColour)
+	{
+		this.hairColour = hairColour;
+	}
+	
+	public Date getBirthDate()
+	{
+		return birthDate;
+	}
+	
+	public void setBirthDate(Date birthDate)
+	{
+		this.birthDate = birthDate;
+	}
+	
+	public int getAge()
+	{
+		// TODO calculate age from DOB
+		throw new UnsupportedOperationException();
 	}
 	
 }
---
+++ src/edu/asu/ser322/data/Gender.java
@@ -0,0 +1,9 @@
+package edu.asu.ser322.data;
+
+public enum Gender
+{
+	MALE,
+	FEMALE,
+	BENT,
+	MIXED;
+}
