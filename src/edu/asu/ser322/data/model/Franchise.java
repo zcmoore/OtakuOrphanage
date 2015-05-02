46c0848104d8df4e109ecbb3b3b42561158403cc
Zach Moore <zcmoore@asu.edu>
2015-05-02 12:42:12 -0700

    Accessors + Mutators

--- src/edu/asu/ser322/data/model/Franchise.java
+++ src/edu/asu/ser322/data/model/Franchise.java
@@ -3,10 +3,10 @@ package edu.asu.ser322.data.model;
 /**
  * A franchise represents a collection of related series and seasons. The members in a
  * franchise may be different seasons of the same series, or different, but related
- * series. 
+ * series.
  * <p>
- * For instance, <a href="http://en.wikipedia.org/wiki/Chaos;Head">Chaos;Head</a>
- * and <a href="http://en.wikipedia.org/wiki/Steins;Gate>Steins;Gate</a> are in the same
+ * For instance, <a href="http://en.wikipedia.org/wiki/Chaos;Head">Chaos;Head</a> and <a
+ * href="http://en.wikipedia.org/wiki/Steins;Gate>Steins;Gate</a> are in the same
  * franchise, but are two different series/shows.
  * 
  * @author Moore, Zachary
@@ -16,5 +16,25 @@ public class Franchise
 {
 	private int id;
 	private String name;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 	
 }
