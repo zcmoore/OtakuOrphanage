d83818f1c6fc3c9aaa52d1ef6488f5000afaf8b6
Zach Moore <zcmoore@asu.edu>
2015-05-02 10:22:27 -0700

    Model Updates

--- src/edu/asu/ser322/data/model/Franchise.java
+++ src/edu/asu/ser322/data/model/Franchise.java
@@ -1,6 +1,20 @@
 package edu.asu.ser322.data.model;
 
+/**
+ * A franchise represents a collection of related series and seasons. The members in a
+ * franchise may be different seasons of the same series, or different, but related
+ * series. 
+ * <p>
+ * For instance, <a href="http://en.wikipedia.org/wiki/Chaos;Head">Chaos;Head</a>
+ * and <a href="http://en.wikipedia.org/wiki/Steins;Gate>Steins;Gate</a> are in the same
+ * franchise, but are two different series/shows.
+ * 
+ * @author Moore, Zachary
+ *
+ */
 public class Franchise
 {
+	private int id;
+	private String name;
 	
 }
