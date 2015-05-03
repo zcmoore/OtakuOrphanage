3f24bebc5b68b42f4cbb044e12df3a9d8a23e1a8
bpaothat <benjaminpaothatat@Ericas-MacBook-Pro.local>
2015-05-03 16:44:33 -0700

    Updated Season

--- src/edu/asu/ser322/data/model/Season.java
+++ src/edu/asu/ser322/data/model/Season.java
@@ -4,29 +4,44 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Model of a season, which belongs to a series and consist of episodes
+ * Model of a season, which consist of episodes
  * 
  * @author Benjamin Paothatat
  *
  */
 		
 public class Season {
-    private int seriesId;
+	public static final Season NULL_Season = new Season();
+	
+    private String seriesName;
     private int seasonNumber;
     private String name;
     private Date airDate;
     private Date finishDate;
+    private String Genre;
+    private String appropriateness;
     
     private List<Episode> episodes;
     
-	public int getSeriesId() 
+    public Season()
+    {
+        super();	
+    }
+    
+    public Season(String id, int seasonNumber)
+    {
+    	this.seriesName = id;
+    	this.seasonNumber = seasonNumber;
+    }
+    
+	public String getSeriesId() 
 	{
-		return seriesId;
+		return seriesName;
 	}
 	
-	public void setSeriesId(int seriesId) 
+	public void setSeriesId(String seriesId) 
 	{
-		this.seriesId = seriesId;
+		this.seriesName = seriesId;
 	}
 	
 	public int getSeasonNumber() 
@@ -77,6 +92,26 @@ public class Season {
 	public void setEpisodes(List<Episode> episodes)
 	{
 		this.episodes = episodes;
+	}
+
+	public String getGenre() 
+	{
+		return Genre;
+	}
+
+	public void setGenre(String genre) 
+	{
+		Genre = genre;
+	}
+
+	public String getAppropriateness() 
+	{
+		return appropriateness;
+	}
+
+	public void setAppropriateness(String appropriateness) 
+	{
+		this.appropriateness = appropriateness;
 	}
     
     
