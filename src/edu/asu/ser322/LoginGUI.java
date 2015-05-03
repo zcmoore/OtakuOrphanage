290b61b05802fcbeaf7a44dc81bf70e6bf7f7fc2
COsorio11 <cosorio@asu.edu>
2015-05-03 10:10:28 -0700

    Styled, Formated, buttons still need to have image

--- src/edu/asu/ser322/LoginGUI.java
+++ src/edu/asu/ser322/LoginGUI.java
@@ -1,8 +1,14 @@
 package edu.asu.ser322;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,15 +28,18 @@ import edu.asu.ser322.data.model.User;
 @SuppressWarnings("serial")
 public class LoginGUI extends JPanel // implements ActionListener
 {
-	protected JLabel animeDatabaseLogin;
-	protected JLabel userLabel;
-	protected JLabel passwordLabel;
-	protected JTextField usernameTextField;
-	protected JPasswordField passwordTextField;
-	protected JButton loginButton;
-	protected JButton registerButton;
+	private JLabel animeDatabaseLogin;
+	private JLabel userLabel;
+	private JLabel passwordLabel;
+	private JTextField usernameTextField;
+	private JPasswordField passwordTextField;
+	private JButton loginButton;
+	private JButton registerButton;
 	private UserDao userDao;
 	public static final String MAINMENU = "1";
+	
+	private BufferedImage img;
+	
 	Client client;
 	MainMenuGUI mainMenu;
 
@@ -46,38 +55,45 @@ public class LoginGUI extends JPanel // implements ActionListener
 	
 	private void init()
 	{
+		setOpaque(false);
+    	addImageBackGround();
+    	
 		userDao = DAOCollection.getUserDao();
 		
-		animeDatabaseLogin = new JLabel("Anime Database");
-		userLabel = new JLabel("Username: ");
+		String animeDatabaseLoginText = "<html>\n" + "<font size=+4><font color=#00FFFF>Otaku Orphanage</font>";
+		String userLabelText = "<html>\n" + "<font size=+2><font color=red>Username:</font>";
+		String passwordLabelText = "<html>\n" + "<font size=+2><font color=red>Password:</font>";
+		
+		
+		animeDatabaseLogin = new JLabel(animeDatabaseLoginText);
+		userLabel = new JLabel(userLabelText);
 		usernameTextField = new JTextField();
-		passwordLabel = new JLabel("Password: ");
+		passwordLabel = new JLabel(passwordLabelText);
 		passwordTextField = new JPasswordField();
 		loginButton = new JButton("Login");
 		registerButton = new JButton("Register");
-		
 	}
 	
 	public void layout()
 	{
 		setLayout(null);
 		
-		animeDatabaseLogin.setBounds(600, 20, 300, 30);
+		animeDatabaseLogin.setBounds(480, 20, 500, 80);
 		add(animeDatabaseLogin);
 		
-		userLabel.setBounds(510, 200, 100, 30);
+		userLabel.setBounds(100, 180, 130, 30);
 		add(userLabel);
 		
-		usernameTextField.setBounds(600, 200, 200, 30);
+		usernameTextField.setBounds(240, 180, 200, 30);
 		add(usernameTextField);
 		
-		passwordLabel.setBounds(510, 250, 100, 30);
+		passwordLabel.setBounds(100, 220, 130, 30);
 		add(passwordLabel);
 		
-		passwordTextField.setBounds(600, 250, 200, 30);
+		passwordTextField.setBounds(240, 220, 200, 30);
 		add(passwordTextField);
 		//FIXME: Implement the button differently
-		loginButton.setBounds(300, 500, 150, 75);
+		loginButton.setBounds(900, 580, 150, 75);
 		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e)
 			{
@@ -89,9 +105,35 @@ public class LoginGUI extends JPanel // implements ActionListener
 		});
 		add(loginButton);
 		
-		registerButton.setBounds(900, 500, 150, 75);
+		registerButton.setBounds(1100, 580, 150, 75);
 		//TODO: Created register panel or take information from login page?
 		// registerButton.addActionListener(this);
 		add(registerButton);
 	}
+	
+	public void addImageBackGround()
+    {
+        try 
+        {
+            img = ImageIO.read(new File("rec/LoginBackground.jpg"));
+        } 
+        catch (FileNotFoundException e)
+        {
+        	 e.printStackTrace();
+        }
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) 
+    {
+    	super.paintComponent(g); 
+        if (img != null)
+        {
+          g.drawImage(img, 0,0,this.getWidth(),this.getHeight(),this);
+        }
+    }
 }
