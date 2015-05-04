63126b55c4aa511e2a3fcf4cffaeb5621df36d52
bpaothat <benjaminpaothatat@Ericas-MacBook-Pro.local>
2015-05-04 14:41:00 -0700

    Added StudioDao

---
+++ src/edu/asu/ser322/data/access/StudioDao.java
@@ -0,0 +1,74 @@
+package edu.asu.ser322.data.access;
+
+import edu.asu.ser322.data.model.Studio;
+import edu.asu.ser322.data.model.User;
+/**
+ * 
+ * @author Benjamin Paothatat
+ *
+ */
+public interface StudioDao 
+{
+	/**
+	 * Add a studio to the persistent store
+	 * 
+	 * @param studio
+	 *            Studio data to add. Must be valid
+	 * @return true if the studio was added successfully
+	 */
+	public boolean addStudio(Studio studio);
+	
+	/**
+	 * Updates a studio in the persistent store
+	 * 
+	 * @param studio
+	 *            Target to update.
+	 * @return true if the user was updated successfully. False if the update failed
+	 */
+	public boolean updateStudio(Studio studio);
+	
+	/**
+	 * Finds the studio with the given studioname, and loads it from the persistent store
+	 * 
+	 * @param studioname
+	 *            Target studio's studioname
+	 * @return the Studio object, or {@link User#NULL_STUDIO} if a studio cannot be found
+	 */
+	public Studio findStudio(String studioname);
+	
+	/**
+	 * Checks that a studio is in the system
+	 * 
+	 * @param studioname
+	 * @return true if a user with the specified studioname exists
+	 */
+	public boolean studioExists(String studioname);
+	
+	/**
+	 * Deletes a studio with the specified studioname
+	 * 
+	 * @param studio
+	 *            Target studio's studioname
+	 * @return true if the deletion was successful. False otherwise
+	 */
+	public boolean deleteStudio(String studioname);
+	
+	/**
+	 * Adds an employee to the persistent store
+	 * 
+	 * @param studioname personname
+	 *            Target studio's name and person' name
+	 * @return true if the deletion was successful. False otherwise
+	 */
+	public boolean addEmployee(String studioname, String personname);
+	
+	/**
+	 * Removes an employee from the persistent store
+	 * 
+	 * @param studioname personname
+	 *            Target studio's name and person' name
+	 * @return true if the deletion was successful. False otherwise
+	 */
+	public boolean removeEmployee(String studioname, String personname);
+	
+}
