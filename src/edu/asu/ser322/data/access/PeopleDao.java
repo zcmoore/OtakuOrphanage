dc760d9d7d67a055955882ec109b14fb6b92d706
bpaothat <benjaminpaothatat@Ericas-MacBook-Pro.local>
2015-05-04 15:03:12 -0700

    Added PeopleDao

---
+++ src/edu/asu/ser322/data/access/PeopleDao.java
@@ -0,0 +1,39 @@
+package edu.asu.ser322.data.access;
+
+import java.util.List;
+
+import edu.asu.ser322.data.model.Person;
+/**
+ * 
+ * @author Benjamin Paothatat
+ *
+ */
+public interface PeopleDao 
+{
+	/**
+	 * Add a person to the persistent store
+	 * 
+	 * @param person
+	 *            Person data to add. Must be valid
+	 * @return true if the person was added successfully
+	 */
+	public boolean addPerson(String name);
+	
+	
+	/**
+	 * Finds the people with the given name, and loads it from the persistent store
+	 * 
+	 * @param name
+	 *            Target people's name
+	 * @return the List of persons, or an empty list
+	 */
+	public List<Person> findPerson(String name);
+	
+	/**
+	 * Checks that a person is in the system
+	 * 
+	 * @param name
+	 * @return true if a person with the specified name exists
+	 */
+	public boolean personExists(String name);
+}
