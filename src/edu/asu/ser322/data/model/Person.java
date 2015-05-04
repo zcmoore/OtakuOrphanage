883ec2bb9d2edcfad4ba362f27f881e9674a3f23
bpaothat <benjaminpaothatat@Ericas-MacBook-Pro.local>
2015-05-04 15:16:28 -0700

    Updated People to Person

--- src/edu/asu/ser322/data/model/Person.java
+++ src/edu/asu/ser322/data/model/Person.java
@@ -1,6 +1,31 @@
 package edu.asu.ser322.data.model;
-
-public class Person
+/**
+ * 
+ * @author Benjamin Paothatat
+ *
+ */
+public class Person 
 {
+    private int personId;
+    private String name;
+    public int getPersonId()
+    {
+		return personId;
+	}
+    
+	public void setPersonId(int personId) 
+	{
+		this.personId = personId;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
 	
 }
