808b7add0b16abb56618245829b485d2697d4e67
Zach Moore <zcmoore@asu.edu>
2015-05-06 11:57:28 -0700

    Update Remove Specification

--- src/edu/asu/ser322/data/access/StudioDao.java
+++ src/edu/asu/ser322/data/access/StudioDao.java
@@ -2,13 +2,14 @@ package edu.asu.ser322.data.access;
 
 import java.util.List;
 
+import edu.asu.ser322.data.model.Person;
 import edu.asu.ser322.data.model.Studio;
 import edu.asu.ser322.data.model.User;
 
 /**
  * 
  * @author Benjamin Paothatat
- * @author Moor, Zachary
+ * @author Moore, Zachary
  *
  */
 public interface StudioDao
@@ -38,7 +39,7 @@ public interface StudioDao
 	 *            Target studio's studioname
 	 * @return the Studio object, or {@link User#NULL_STUDIO} if a studio cannot be found
 	 */
-	public Studio findStudio(String studioname);
+	public Studio findStudio(String studioName);
 	
 	/**
 	 * Checks that a studio is in the system
@@ -46,7 +47,7 @@ public interface StudioDao
 	 * @param studioname
 	 * @return true if a user with the specified studioname exists
 	 */
-	public boolean studioExists(String studioname);
+	public boolean studioExists(String studioName);
 	
 	/**
 	 * Deletes a studio with the specified studioname
@@ -55,26 +56,60 @@ public interface StudioDao
 	 *            Target studio's studioname
 	 * @return true if the deletion was successful. False otherwise
 	 */
-	public boolean deleteStudio(String studioname);
+	public boolean deleteStudio(String studioName);
 	
 	/**
-	 * Adds an employee to the persistent store
+	 * Adds an employee to the persistent store by name. If there is no person registered
+	 * with the given name, a new person will be created. If there are multiple persons
+	 * with the same name, this method will fail. In the latter case, use
+	 * {@link #addEmployee(String, Person, String)}
 	 * 
-	 * @param studioname
-	 *            personname Target studio's name and person' name
-	 * @return true if the deletion was successful. False otherwise
+	 * @param studioName
+	 *            Employer's name
+	 * @param employee
+	 *            New Employee's name
+	 * @return true if the addition was successful. False otherwise
 	 */
-	public boolean addEmployee(String studioname, String personname, String role);
+	public boolean addEmployee(String studioName, String personName, String role);
 	
 	/**
-	 * Removes an employee from the persistent store
+	 * Removes all employees with the given name and role who are employed by the given
+	 * studio.
 	 * 
-	 * @param studioname
-	 *            personname Target studio's name and person' name
+	 * @param studioName
+	 *            Employer's name
+	 * @param personName
+	 *            Target Employee's name
 	 * @return true if the deletion was successful. False otherwise
 	 */
-	public boolean removeEmployee(String studioname, String personname, String role);
+	public boolean removeEmployees(String studioName, String personName, String role);
 	
-	List<Studio> listAll();
+	public List<Studio> listAll();
+	
+	/**
+	 * Adds an employee to the persistent store by {@link Person} reference. The
+	 * {@link Person} must be valid, and should be obtained via
+	 * {@link PeopleDao#findPerson(String)} or another Dao that returns {@link People}
+	 * results.
+	 * 
+	 * @param studioName
+	 *            Employer's name
+	 * @param employee
+	 *            New Employee's reference (depends on id)
+	 * @return true if the addition was successful. False otherwise
+	 */
+	public boolean addEmployee(String studioName, Person employee, String role);
+	
+	/**
+	 * Removes an employee from the persistent store who matches the given studio,
+	 * personID, and role.
+	 * 
+	 * @param studioName
+	 *            Employer's name
+	 * @param personName
+	 *            Target Employee's reference (depends on id)
+	 * @return true if the deletion was successful. False otherwise
+	 */
+	public boolean removeEmployee(String studioName, Person employee, String role);
 	
 }
